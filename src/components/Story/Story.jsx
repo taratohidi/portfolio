c41f1021b1b20c8ae80fd5f030b4398f44505964
Tara Tohidi <tara.tohidi@gmail.com>
2022-08-11 12:01:42 -0700

    Creat and style of 1st version of story content

--- src/components/Story/Story.jsx
+++ src/components/Story/Story.jsx
@@ -5,7 +5,22 @@ function Story() {
   return (
     <>
       <div className="story__picture">story picture</div>
-      <div className="story__content">story content</div>
+      <div className="story__content">
+        <h3 className="heading-3 mb-sm">Happy Customers</h3>
+        <h2 className="heading-2 heading-2--dark mb-md">
+          &ldquo;Best decision of my life&ldquo;
+        </h2>
+        <p className="story__text mb-lg">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio
+          molestiae voluptas, adipisci dolores nesciunt quibusdam quisquam
+          aperiam maiores odit, deserunt debitis consectetur sunt quidem rerum,
+          quae facilis? Earum, architecto tempore!
+        </p>
+        <a href="/" className="btn">
+          {" "}
+          find my project
+        </a>
+      </div>
     </>
   );
 }
