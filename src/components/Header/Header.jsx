08d275fdc2ae1890afd89f2b4d8e9343e9aafca4
Tara Tohidi <tara.tohidi@gmail.com>
2022-08-12 17:26:37 -0700

    create and style header and skills section

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,41 +1,57 @@
 import "./Header.scss";
-
 import React from "react";
+
+// Import Icons
+import sass from "../../assets/icon/logo-sass.svg";
+import express from "../../assets/icon/logo-express.svg";
+import mongo from "../../assets/icon/mongodb-icon.svg";
+import js from "../../assets/icon/logo-javascript.svg";
 
 function Header() {
   return (
     <header className="header">
-      <h2 className="header__logo">TARA TOHIDI</h2>
+      <h3 className="heading-3 header__logo">TARA TOHIDI</h3>
       <nav className="header__nav">
         <li className="header__nav--list">
           <a href="/" className="header__nav--link">
-            link 1
+            about
           </a>
         </li>
         <li className="header__nav--list">
           <a href="/" className="header__nav--link">
-            link 2
+            projects
           </a>
         </li>
         <li className="header__nav--list">
           <a href="/" className="header__nav--link">
-            link 3
+            contact
           </a>
         </li>
         <li className="header__nav--list">
           <a href="/" className="header__nav--link">
-            link 4
+            resume
+          </a>
+        </li>
+        <li className="header__nav--list">
+          <a href="/" className="header__nav--link">
+            resources
           </a>
         </li>
       </nav>
 
-      <h3>This is Tara!</h3>
-      <h1>full stack developer</h1>
-      <a href="/" className="btn">
+      <h2 className="heading-2 header__heading-2 ">This is Tara!</h2>
+      <h1 className="heading-1">full stack developer</h1>
+      <a href="/" className="btn header__btn">
         view my projects
       </a>
 
-      <div className="header__seenon-text">Other Skills</div>
+      <div className="header__skills-text">Other Skills</div>
+      <div className="header__skills-logos">
+        <img src={js} alt="java script" className="header__skills-icon" />
+        <img src={express} alt="express.js" className="header__skills-icon" />
+        <img src={mongo} alt="mongodb" className="header__skills-icon" />
+        <img src={sass} alt="sass" className="header__skills-icon" />
+      </div>
     </header>
   );
 }
--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,7 +1,31 @@
 import "./Skills.scss";
 import React from "react";
 
+// Import Icons
+import react from "../../assets/icon/logo-react.svg";
+import nodejs from "../../assets/icon/logo-nodejs.svg";
+import mysql from "../../assets/icon/mysql-icon.svg";
+
 function Skills() {
-  return <div className="skills">Skills</div>;
+  return (
+    <div className="skills">
+      <h3 className="heading-3 skills__heading">Top 3 skills</h3>
+      <div className="skills__list">
+        <img src={react} alt="react" className="skills__icon" />
+        <div className="skills__detail">
+          <h4 className="heading-4 heading-4--light">React</h4>
+        </div>
+
+        <img src={nodejs} alt="nodejs" className="skills__icon" />
+        <div className="skills__detail">
+          <h4 className="heading-4 heading-4--light">Node.js</h4>
+        </div>
+        <img src={mysql} alt="mysql" className="skills__icon" />
+        <div className="skills__detail">
+          <h4 className="heading-4 heading-4--light">mySQL</h4>
+        </div>
+      </div>
+    </div>
+  );
 }
 export default Skills;
